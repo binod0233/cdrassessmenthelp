cbac2518edf3ea4c49d5223dca394d16d119ff27
Binod Sharma <31683207+binod0233@users.noreply.github.com>
2022-09-21 15:47:28 +0545

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,15 +5,15 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      host: env(
+        "DATABASE_HOST",
+        "heroku-help.c6r19h8f82ti.us-east-1.rds.amazonaws.com"
+      ),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "postgres"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "Copenned123"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
-    debug: false,
   },
 });
